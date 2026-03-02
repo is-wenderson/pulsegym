77d327125c9c1c5576c49d008aaf82e33934d2c8
wenderson <wendersoncunha176@gmail.com>
2026-03-02 19:42:26 -0300

    Estrutura inicial do projeto

---
+++ backend/apps/evo_integration/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EvoIntegrationConfig(AppConfig):
+    name = 'evo_integration'
---
+++ backend/apps/nps_engine/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NpsEngineConfig(AppConfig):
+    name = 'nps_engine'
